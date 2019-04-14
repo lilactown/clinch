546baee1cb2c00401fef9a9d3dab5e32d7cecec6
Will Acton <will@willacton.com>
2019-04-14 10:19:10 -0700

    correct URL

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lilactown/clinch "1.0.0"
   :description "React hooks for ClojureScript"
-  :url "https://github.com/Lokeh/hooks"
+  :url "https://github.com/Lokeh/clinch"
   :license {:name "MIT"}
   :source-paths ["src"])
